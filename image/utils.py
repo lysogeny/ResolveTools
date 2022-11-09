af2c6e4d4a26f64985bb0234d3bc4ca4e67d5ecf
valentinwust <44555270+valentinwust@users.noreply.github.com>
2022-11-09 21:00:01 +0100

    Image stuff

---
+++ image/utils.py
@@ -0,0 +1,16 @@
+from tifffile import imwrite
+
+def save_tiff(filename, data, compression="zlib"):
+    """ Save image stack as tiff.
+    """
+    imwrite(filename, data, metadata={"mode": "composite"}, imagej=True, compression=compression)
+
+from skimage import exposure
+
+def claher(img):
+    """
+    Runs Contrast Limited Adaptive Histogram Equalization (CLAHE) and normalizes to [0,1].
+    """
+    img = exposure.equalize_adapthist(img, kernel_size = 127, clip_limit = 0.01, nbins = 256)
+    img = img / img.max()
+    return img
