7bd8b5ab6da43a9afb44d646e1f402106eda8955
valentinwust <44555270+valentinwust@users.noreply.github.com>
2023-01-23 18:13:18 +0100

    Update visualization.py

--- baysor/visualization.py
+++ baysor/visualization.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from ..segmentation.visualize import get_rgb_distinct
 from ..segmentation.counts import read_loom
 from ..image.utils import read_single_modality_confocal
+from ..utils.parameters import CONFOCAL_VOXEL_SIZE
 
 ##############################
 ### Simple Cell Plot
