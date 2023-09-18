acf3de519d14443c1bdc7324b0c578eb4a47e2e7
Jooa <j.hooli@dkfz.de>
2023-09-18 13:25:44 +0200

    Clean up various functions

--- src/resolve_tools/utils/parameters.py
+++ src/resolve_tools/utils/parameters.py
@@ -1,13 +1,13 @@
 
 ##############################
-### Microscope parameters
+# Microscope parameters
 ##############################
 
 # Size of the tiles resolve uses
 RESOLVE_TILE_SIZE = 2144
 
 # Resolve resoultion
-RESOLVE_VOXEL_SIZE = [0.3125,0.138,0.138,r"$\mu$m","zyx"]
+RESOLVE_VOXEL_SIZE = [0.3125, 0.138, 0.138, r"$\mu$m", "zyx"]
 
 # Confocal microscope resolution
-CONFOCAL_VOXEL_SIZE = [1.,0.142,0.142,r"$\mu$m","zyx"]
+CONFOCAL_VOXEL_SIZE = [1., 0.142, 0.142, r"$\mu$m", "zyx"]
--- src/resolve_tools/utils/utils.py
+++ src/resolve_tools/utils/utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
 
+
 def printwtime(text):
-    print(datetime.now().strftime("%H:%M:%S"),"-",text)
+    """Print a text with a timestamp"""
+    print(datetime.now().strftime("%H:%M:%S"), "-", text)
