f8a53233a69d26c4e3c1972108ca05afac99d03f
Jooa <j.hooli@dkfz.de>
2023-09-18 12:24:50 +0200

    Clean up I/O files

--- src/resolve_tools/image/lif.py
+++ src/resolve_tools/image/lif.py
@@ -1,10 +1,9 @@
-from readlif.reader import LifFile
+"""
+Reading confocal .lif images
+"""
 
-##############################
-### Reading confocal .lif images
-##############################
 
 def read_lif_image(lif, z, channel):
-    """ lif.get_frame apparently uses random ordering, but this works.
+    """lif.get_frame apparently uses random ordering, but this works.
     """
     return lif._get_item(z*lif.channels + channel)
--- src/resolve_tools/image/utils.py
+++ src/resolve_tools/image/utils.py
@@ -1,18 +1,28 @@
-##############################
-### Reading images
-##############################
+"""
+Reading images, saving images and modifying images
+"""
 
+from skimage import exposure
+from tifffile import imwrite
+import numpy as np
+import mclahe as mc
 import cv2
 from tifffile import TiffFile
+
+##############################
+# Reading images
+##############################
+
 
 def read_single_modality_confocal(file, norm=True):
     """ Read single modality confocal .tif image.
     """
-    img = np.asarray(cv2.imreadmulti(file, flags=cv2.IMREAD_ANYDEPTH )[1])
+    img = np.asarray(cv2.imreadmulti(file, flags=cv2.IMREAD_ANYDEPTH)[1])
     if norm:
         return img/img.max()
     else:
         return img
+
 
 def get_single_modality_shape(file):
     """ Get shape of single modality confocal .tif image,
@@ -24,51 +34,54 @@ def get_single_modality_shape(file):
     return shape
 
 ##############################
-### Saving images
+# Saving images
 ##############################
 
-from tifffile import imwrite
 
 def save_tiff(filename, data, compression="zlib"):
     """ Save image stack as tif.
     """
-    imwrite(filename, data, metadata={"mode": "composite"}, imagej=True, compression=compression)
+    imwrite(filename, data, metadata={"mode": "composite"},
+            imagej=True, compression=compression)
+
 
 def save_tiff_from_float(filename, data, compression="zlib"):
     """ save_tiff, with additional converion of the image from [0,1] to 8bit.
     """
-    imwrite(filename, (data*255).astype("H"), metadata={"mode": "composite"}, imagej=True, compression=compression)
+    imwrite(filename, (data*255).astype("H"), metadata={"mode": "composite"},
+            imagej=True, compression=compression)
 
 ##############################
-### Modifying images
+# Modifying images
 ##############################
 
-from skimage import exposure
-import mclahe as mc
 
-def claher(img, kernel_size = 127, clip_limit = 0.01, nbins = 256):
+def claher(img, kernel_size=127, clip_limit=0.01, nbins=256):
     """
-    Runs Contrast Limited Adaptive Histogram Equalization (CLAHE) and normalizes to [0,1].
+    Runs Contrast Limited Adaptive Histogram Equalization (CLAHE)
+    and normalizes to [0,1].
     """
-    img = exposure.equalize_adapthist(img, kernel_size = kernel_size, clip_limit = clip_limit, nbins = nbins)
+    img = exposure.equalize_adapthist(img, kernel_size=kernel_size,
+                                      clip_limit=clip_limit, nbins=nbins)
     img = img / img.max()
     return img
 
-def mclaher(img, kernel_size=[10,128,128],n_bins=128,clip_limit=0.01,adaptive_hist_range=False):
+
+def mclaher(img, kernel_size=[10, 128, 128], n_bins=128, clip_limit=0.01,
+            adaptive_hist_range=False):
     """
-    Runs Contrast Limited Adaptive Histogram Equalization (CLAHE) in 3D and normalizes to [0,1].
+    Runs Contrast Limited Adaptive Histogram Equalization (CLAHE) in 3D
+    and normalizes to [0,1].
     """
-    img = mc.mclahe(img, kernel_size=kernel_size, n_bins=n_bins, clip_limit=clip_limit, adaptive_hist_range=adaptive_hist_range)
+    img = mc.mclahe(img, kernel_size=kernel_size,
+                    n_bins=n_bins, clip_limit=clip_limit,
+                    adaptive_hist_range=adaptive_hist_range)
     img = img / img.max()
     return img
 
-import cv2
-import numpy as np
 
 def resize_shrink(img, factor):
     """ Shrink image by factor along all axes.
     """
-    return cv2.resize(img, np.asarray(img.shape)[::-1]//factor, interpolation = cv2.INTER_AREA)
-
-
-
+    return cv2.resize(img, np.asarray(img.shape)[::-1]//factor,
+                      interpolation=cv2.INTER_AREA)
